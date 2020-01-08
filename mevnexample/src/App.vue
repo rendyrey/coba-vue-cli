35bb720e9a73abb0e695506aa58e9ddd3bd8e941
rendyrey <rendyreynaldy@gmail.com>
2020-01-08 07:34:58 +0700

    tetsetws

---
+++ mevnexample/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container">
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <router-link to="/" class="nav-link">Home</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/create" class="nav-link">Create Post</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/posts" class="nav-link">Posts</router-link>
+        </li>
+      </ul>
+    </nav><br />
+    <transition name="fade">
+      <router-view></router-view>
+    </transition>
+  </div>
+</template>
+
+<style>
+    /* .fade-enter-active, .fade-leave-active {
+      transition: opacity .5s
+    } */
+    .fade-enter-active{
+      transition: opacity .5s
+    }
+    .fade-enter, .fade-leave-active {
+      opacity: 0
+    }
+    .nav-item{
+      transition:.2s
+    }
+    .nav-item:hover{
+      transform:scale(1.2,1.2);
+      transform:rotate(15deg);
+    }
+</style>
+
+<script>
+
+    export default{
+    }
+</script>
