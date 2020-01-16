21e4635d5a80c1eda6f0fa60e2c78e5f839d20a3
rendyrey <rendyreynaldy@gmail.com>
2020-01-16 07:46:10 +0700

    test

--- laravel-vue-crud/resources/views/company/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="container" id="app">
-	{{-- <index-company></index-company> --}}
-	{{-- <router-view name="company-index"></router-view> --}}
-	{{-- <router-view name=""></router-view> --}}
-</div>
-@endsection
